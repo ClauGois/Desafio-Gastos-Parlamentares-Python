a890d05a1d5a91a9b78dbb3d06fb0b25c3d2076c
Clau <Clau@hotmail.com>
2022-07-22 17:23:14 -0300

    respostas 1

---
+++ perguntas.py
@@ -0,0 +1,19 @@
+class perguntas:
+    def pergunta1(self):
+        print('1 - Qual estado e partido têm a maior soma do valor líquido nos anos de 2009 e 2020?')
+        print('\n')
+    def pergunta2(self):
+        print('2 - Qual é o maior número da carteira parlamentar em SC ?')
+        print('\n')
+    def pergunta3(self):
+        print('3 - Entre 01/01/2019 e 30/06/2020, nos estados de SP, RJ e SC, qual a quantidade de registros em cada estado para o período indicado?')
+        print('\n')
+    def pergunta4(self):
+        print('4 - Considerando o ano de 2019, quais foram os 5 partidos que mais gastaram (valor líquido) '
+        'com serviço de táxi e que tiveram pelo menos uma restituição nesse período? (Apresentar'
+        'ambos os valores limitados a duas casas decimais)')
+        print('\n')
+    def pergunta5(self):
+        print('5 - Considerando o ano de emissão como sendo 2019 e 2020, exiba as 3 siglas partidárias que'
+        'mais gastaram cota parlamentar (valores líquidos) para trechos que contenham "POA" ou "FLN".')
+        print('\n')
